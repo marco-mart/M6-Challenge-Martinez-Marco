5bb15e856a11c6115dd6187b74c7ff8a2d79807c
marco-mart <martinez.marco.1027@gmail.com>
2023-02-21 14:48:34 -0700

    Create test for POST HTTP method. Need to validate correct object.

--- customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
+++ customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
@@ -1,5 +1,13 @@
 package com.company.customerdataservice.controller;
 
+import com.company.customerdataservice.model.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController
 {
     /**
@@ -10,6 +18,12 @@ public class CustomerController
      *
      * Response Status: 201 Created
      */
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody Customer customer)
+    {
+        return null;
+    }
 
 
     /**
