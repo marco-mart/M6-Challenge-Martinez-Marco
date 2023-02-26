8f7a8b71c5b6502a2223a78fada6bef82a2195cc
marco-mart <martinez.marco.1027@gmail.com>
2023-02-26 15:35:34 -0700

    Added all tests. Controller testing with MockMvc does not seem to work with repo in Controller.

--- customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
+++ customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
@@ -1,15 +1,21 @@
 package com.company.customerdataservice.controller;
 
 import com.company.customerdataservice.model.Customer;
+import com.company.customerdataservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CustomerController
 {
+
+//    @Autowired
+//    CustomerRepository customerRepo;
+
     /**
      * HTTP Method: POST
      * Endpoint: "/customers"
@@ -22,18 +28,24 @@ public class CustomerController
     @ResponseStatus(HttpStatus.CREATED)
     public Customer addCustomer(@RequestBody Customer customer)
     {
-        return null;
+        return customer;// customerRepo.save(customer);
     }
 
 
     /**
      * HTTP Method: PUT
-     * Endpoint: "/customers/"
+     * Endpoint: "/customers"
      * Request body: updated Customer object
-     * Response body: newly updated Customer object
+     * Response body: n/a
      *
      * Response Status: 204 No Content
      */
+    @PutMapping("/customers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@RequestBody Customer updatedCustomer)
+    {
+        // customerRepo.save(updatedCustomer);
+    }
 
     /**
      * HTTP Method: DELETE
@@ -43,6 +55,13 @@ public class CustomerController
      *
      * Response Status: 204 No Content
      */
+    @DeleteMapping("/customers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@RequestBody Customer customer)
+    {
+        // customerRepo.delete(customer);
+    }
+
 
     /**
      * HTTP Method: GET
@@ -52,6 +71,14 @@ public class CustomerController
      *
      * Response Status: 200 Ok
      */
+    @GetMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Customer getCustomerById(@PathVariable int id)
+    {
+        // Optional<Customer> result =  customerRepo.findById(id);
+
+        return null;// result.isPresent() ? result.get() : null;
+    }
 
     /**
      * HTTP Method: GET
@@ -61,4 +88,11 @@ public class CustomerController
      *
      * Response Status: 200 Ok
      */
+    @GetMapping("/customers/state/{state}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Customer> getCustomersByState(@PathVariable String state)
+    {
+        // customerRepo may return an empty list
+        return null;// customerRepo.findByState(state);
+    }
 }
